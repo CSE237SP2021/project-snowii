4bb2c8a905ebf13ea88349ce9dfae15a6ab7b11e
jtinaguo <jtinaguo@Laptop.localdomain>
2021-04-28 12:32:21 -0700

    Add functionality

--- src/cse237/Snowii.java
+++ src/cse237/Snowii.java
@@ -16,7 +16,7 @@ public class Snowii {
 			snowii.printDefaultTable(resorts, resort_exts);
 		}
 
-		//Create a list of user-inputed resort
+		// Create a list of user-inputed resort
 		else if (args.length == 2) {
 			Resort userResort = new Resort(snowii.getUrlName(args, snowii));
 			// Print info to terminal
@@ -25,12 +25,18 @@ public class Snowii {
 	}
 
 	public String getStateName(String[] args) {
-		String stateName = args[0].toLowerCase();
+		String stateName = beautifySnowiiName(args[0]);
 		return stateName;
 	}
-	
+
+	public String beautifySnowiiName(String name) {
+		name.toLowerCase();
+		name = name.replace(" ", "-");
+		return name;
+	}
+
 	public String getResortName(String[] args) {
-		String resortName = args[1];
+		String resortName = beautifySnowiiName(args[1];
 		return resortName;
 	}
 
@@ -38,19 +44,18 @@ public class Snowii {
 		String urlName = snowii.getStateName(args) + "/" + snowii.getResortName(args);
 		return urlName;
 	}
-	
+
 	public void printDefaultTable(List<Resort> resorts, String[] resort_exts) {
 		System.out.println("Snowii: 7-day snow forecast");
-		for(int i  = 0; i < resorts.size(); i++) {
+		for (int i = 0; i < resorts.size(); i++) {
 			Resort resort = resorts.get(i);
 			System.out.println("Current resort: " + resort_exts[i]);
 			try {
 				int[] data = resort.getSnowData();
 				for (int j = 0; j < data.length; j++) {
-					System.out.println("Day " + (j+1) + ": " + data[j] + " inches");
+					System.out.println("Day " + (j + 1) + ": " + data[j] + " inches");
 				}
-			} 
-			catch(Exception e) {
+			} catch (Exception e) {
 				System.out.println("An exception occurred while fetching snow data: " + e.getMessage());
 			}
 		}
@@ -62,34 +67,25 @@ public class Snowii {
 		try {
 			int[] data = definedResort.getSnowData();
 			for (int j = 0; j < data.length; j++) {
-				System.out.println("Day " + (j+1) + ": " + data[j] + " inches");
+				System.out.println("Day " + (j + 1) + ": " + data[j] + " inches");
 			}
-		} 
-		catch(Exception e) {
+		} catch (Exception e) {
 			System.out.println("An exception occurred while fetching snow data: " + e.getMessage());
-		}    
+		}
 	}
 
 	public String[] createDefaultTable(List<Resort> resorts) {
-		String resort_exts[] = new String[]{"wyoming/jackson-hole",
-				"colorado/vail",
-				"colorado/beaver-creek",
-				"colorado/breckenridge",
-				"colorado/keystone",
-				"colorado/crested-butte-mountain-resort",
-				"colorado/telluride",
-				"colorado/silverton-mountain",
-				"utah/park-city-mountain-resort",
-				"utah/snowbasin",
-				"british-columbia/whistler-blackcomb"
-		};
+		String resort_exts[] = new String[] { "wyoming/jackson-hole", "colorado/vail", "colorado/beaver-creek",
+				"colorado/breckenridge", "colorado/keystone", "colorado/crested-butte-mountain-resort",
+				"colorado/telluride", "colorado/silverton-mountain", "utah/park-city-mountain-resort", "utah/snowbasin",
+				"british-columbia/whistler-blackcomb" };
 
 		addResort(resorts, resort_exts);
 		return resort_exts;
 	}
 
 	public void addResort(List<Resort> resorts, String[] resort_exts) {
-		for(int i = 0; i < resort_exts.length; i++) {
+		for (int i = 0; i < resort_exts.length; i++) {
 			Resort resort = new Resort(resort_exts[i]);
 			resorts.add(resort);
 		}
